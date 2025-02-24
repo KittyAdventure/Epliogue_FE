f255ddb97b83ae7f536d86701aa6ebcc746ebbdd
min-miny <myp7410@gmail.com>
2025-02-24 12:08:37 +0900

    환경설정

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import AppRouter from './routes/AppRouter';
+
+const App = (): React.JSX.Element => {
+  return <AppRouter />;
+};
+
+export default App;
---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import MainPage from '../pages/MainPage';
+// import Header from '../components/layout/Header';
+// import Footer from '../components/layout/Footer';
+
+const AppRouter = (): React.JSX.Element => {
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
