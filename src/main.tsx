b59126ff463349df2cc8d211e5d84137e8c78557
cupnoodles37 <roopre48@daum.net>
2025-02-25 12:30:43 +0900

    feat : favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+import './assets/css/index.css';
 import './assets/css/layout.css';
 import './assets/css/reset.css';
 
