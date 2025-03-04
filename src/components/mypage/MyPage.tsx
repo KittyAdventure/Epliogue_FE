81fec091264b3e12274c32a93cac27562eb09e79
min-miny <myp7410@gmail.com>
2025-03-05 00:29:55 +0900

    :lipstick:style: mypage layout progress

--- src/components/mypage/MyPage.tsx
+++ src/components/mypage/MyPage.tsx
@@ -1,23 +1,31 @@
-
-import PageTabs from "./PageTabs"
+import PageTabs from './PageTabs';
 
 // 마이페이지 클릭 -> 로그인 되어있음 -> 이 페이지로 온다
 const MyPage = (): React.JSX.Element => {
   return (
     <div className="mypage ">
-      <h2 className="title">마이페이지</h2>
-      <div className="contentWrap">
-        <aside>
-          <div>Avatar</div>
-          <h4>Username</h4>
-          <p>email@gmail.com</p>
-          <ul>
-            <li>Followers:000</li>
-            <li>Following:000</li>
-          </ul>
-          <div>Calendar</div>
+      <h2 className="title my-0 mx-[auto] max-w-[1440px] py-20 text-4xl font-medium">
+        마이페이지
+      </h2>
+      <div className="contentWrap flex justify-between my-0 mx-[auto] max-w-[1440px]">
+        <aside className="border border-red-500 text-center w-1/5">
+          <div className="my-0 mx-[auto] w-40 h-40 border border-green-500 rounded-full">
+            Avatar
+          </div>
+          <div className="user-info mt-5">
+            <h4 className="text-xl font-medium">Username</h4>
+            <p className="text-[#aaa]">username247@gmail.com</p>
+            <ul className="follow-info flex justify-center items-center mt-3">
+              <li className="mr-3">Followers:000</li>
+              <span> | </span>
+              <li className="ml-3">Following:000</li>
+            </ul>
+          </div>
+          <div className="calendar mt-10 w-auto h-96 border border-green-500">
+            Calendar
+          </div>
         </aside>
-        <div className="content">
+        <div className="content w-4/5">
           <PageTabs />
         </div>
       </div>
--- src/components/mypage/PageTabs.tsx
+++ src/components/mypage/PageTabs.tsx
@@ -6,59 +6,12 @@
 const PageTabs = (): React.JSX.Element => {
   return (
     <div>
-      {/* Tab선택 */}
-      {/* <div id="tab1" className="activeTab">
-        <h4>리뷰</h4>
-        <p>51</p>
-      </div>
-      <div id="tab2">
-        <h4>모임</h4>
-        <p>11</p>
-      </div>
-      <div id="tab3">
-        <h4>컬렉션</h4>
-        <p>7</p>
-      </div>
-      <div id="tab4">
-        <h4>포인트</h4>
-        <p>99000</p>
-      </div> */}
-      {/* Tab 선택에 따라 보여지는 콘텐츠 */}
-      <div role="tablist" className="tabs tabs-bordered">
-        <input
-          type="radio"
-          name="my_tabs_1"
-          role="tab"
-          className="tab"
-          aria-label="Tab 1"
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          Tab content 1
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_1"
-          role="tab"
-          className="tab"
-          aria-label="Tab 2"
-          defaultChecked
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          Tab content 2
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_1"
-          role="tab"
-          className="tab"
-          aria-label="Tab 3"
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          Tab content 3
-        </div>
-      </div>
+      <ul>
+        <li>Tab1</li>
+        <li>Tab2</li>
+        <li>Tab3</li>
+        <li>Tab4</li>
+      </ul>
     </div>
   );
 };
