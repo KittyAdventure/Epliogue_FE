ac975b450feb5926cf18b78a0bbd93e61f6d3fc0
cupnoodles37 <roopre48@daum.net>
2025-03-11 16:36:52 +0900

    :sparkles: feat: book page 수정

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { FaFilter, FaStar } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 interface Book {
@@ -15,24 +16,112 @@ interface Book {
 
 const BookPage: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
-  const [selectedBooks, setSelectedBooks] = useState<Set<string>>(new Set());
-  const [sortCriterion, setSortCriterion] = useState<string>('latest');
-  const [createdAt, setCreatedAt] = useState<number[]>([]); // 책 등록일 저장용 상태
+  const [sortCriterion, setSortCriterion] = useState<string>('latest'); // For sorting
+  const [filter, setFilter] = useState<string>(''); // For rating filter
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [createdAt, setCreatedAt] = useState<number[]>([]);
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
+  const [selectedLetter, setSelectedLetter] = useState<string | null>(null);
+
+  const englishLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const koreanLetters = [
+    'ㄱ',
+    'ㄴ',
+    'ㄷ',
+    'ㄹ',
+    'ㅁ',
+    'ㅂ',
+    'ㅅ',
+    'ㅇ',
+    'ㅈ',
+    'ㅊ',
+    'ㅋ',
+    'ㅌ',
+    'ㅍ',
+    'ㅎ',
+  ];
+
+  const options = [
+    {
+      value: '5',
+      label: (
+        <>
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />
+        </>
+      ),
+    },
+    {
+      value: '4',
+      label: (
+        <>
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />
+        </>
+      ),
+    },
+    {
+      value: '3',
+      label: (
+        <>
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />
+        </>
+      ),
+    },
+    {
+      value: '2',
+      label: (
+        <>
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />
+        </>
+      ),
+    },
+    {
+      value: '1',
+      label: (
+        <>
+          <FaStar className="text-[#FACC15]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />{' '}
+          <FaStar className="text-[#D1D1D1]" />
+        </>
+      ),
+    },
+  ];
+
+  const options2 = [
+    { value: 'latest', label: '최신순' },
+    { value: 'list', label: '조회순' },
+    { value: 'star', label: '평점순' },
+  ];
+
   const navigate = useNavigate();
 
   useEffect(() => {
     fetch('http://localhost:5000/books')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
         if (data.items) {
           setBooks(data.items);
           const currentTime = new Date().getTime();
-          setCreatedAt(new Array(data.items.length).fill(currentTime)); // 책이 로드된 시각을 저장
+          setCreatedAt(new Array(data.items.length).fill(currentTime));
         }
       })
       .catch((error) => {
@@ -41,112 +130,172 @@ const BookPage: React.FC = () => {
       });
   }, []);
 
-  // 책 정렬 함수
   const sortBooks = (books: Book[], createdAt: number[]) => {
     if (sortCriterion === 'latest') {
-      return [...books].sort((a, b) => {
-        // `books`와 `createdAt`을 인덱스 기반으로 함께 비교
-        return createdAt[books.indexOf(b)] - createdAt[books.indexOf(a)]; // 등록일 기준 최신순 정렬 (내림차순)
-      });
+      return [...books].sort(
+        (a, b) => createdAt[books.indexOf(b)] - createdAt[books.indexOf(a)],
+      );
     } else if (sortCriterion === 'pubDate') {
-      return [...books].sort((a, b) => {
-        return new Date(a.pubDate).getTime() - new Date(b.pubDate).getTime(); // 출간일순 정렬 (오름차순)
-      });
+      return [...books].sort(
+        (a, b) => new Date(a.pubDate).getTime() - new Date(b.pubDate).getTime(),
+      );
     }
-    return books; // 기본값은 원래 순서로 반환
-  };
-
-  // 정렬 기준 변경 핸들러
-  const handleSortChange = (criterion: string) => {
-    setSortCriterion(criterion);
-  };
-
-  const toggleBookmark = (isbn: string) => {
-    setSelectedBooks((prev) => {
-      const newSet = new Set(prev);
-      if (newSet.has(isbn)) {
-        newSet.delete(isbn);
-      } else {
-        newSet.add(isbn);
-      }
-      return newSet;
-    });
+    return books;
   };
 
   if (books.length === 0) {
     return <div>Loading...</div>;
   }
 
-  // 책 정렬 후 렌더링
   const sortedBooks = sortBooks(books, createdAt);
 
   return (
     <section className="section-wrap">
       <div className="pt-6 flex flex-col gap-14">
-        <h1 className="text-3xl font-bold mb-6">책 정보 페이지</h1>
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold mb-6">책 정보 페이지</h1>
+          <div className="w-35 z-50">
+            <button
+              className="w-full border p-2 px-4 rounded-lg text-m bg-white flex justify-between items-center hover:"
+              onClick={() => setIsOpen2(!isOpen2)}
+            >
+              {sortCriterion
+                ? options2.find((opt) => opt.value === sortCriterion)?.label
+                : '정렬'}
+              <span>▼</span>
+            </button>
+
+            {isOpen2 && (
+              <ul className="absolute w-35 mt-1 border py-2 border-gray-300 rounded-lg bg-white shadow-md">
+                {options2.map((option2) => (
+                  <li
+                    key={option2.value}
+                    className="py-2 px-4 cursor-pointer hover:font-bold transition-all duration-300"
+                    onClick={() => {
+                      setSortCriterion(option2.value);
+                      setIsOpen2(false);
+                    }}
+                  >
+                    {option2.label}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+
         <div className="flex w-full justify-between gap-[2vw]">
           {/* 정렬 기준 선택 */}
-          <div className="w-1/7 h-[17vh] p-4 bg-gray-100 rounded-lg shadow-md">
-            <div className="flex items-center mb-2 ml-2">
-              <h2 className="text-sm text-[#888888] mr-1">정렬 기준</h2>
-              <i className="fas fa-filter text-[#888888] text-xs translate-y-[1px]"></i>
+          <div className="w-70 p-6 bg-white rounded-xl shadow-lg border border-gray-200">
+            <h3 className="text-lg font-semibold flex items-center mb-4 text-gray-800">
+              필터 <FaFilter className="ml-2 text-gray-600" />
+            </h3>
+            <hr className="mb-4 border-gray-300" />
+
+            {/* 책 제목 필터 */}
+            <h4 className="font-semibold mb-3 text-gray-700">책 제목</h4>
+            <div className="mb-5">
+              <h5 className="font-medium mb-2 text-gray-600">한글</h5>
+              <div className="flex flex-wrap gap-2">
+                {koreanLetters.map((letter) => (
+                  <button
+                    key={letter}
+                    className={`w-10 h-10 flex items-center justify-center text-sm rounded-md transition-colors duration-300 ${
+                      selectedLetter === letter
+                        ? 'bg-blue-500 text-white border border-blue-700'
+                        : 'bg-gray-200 text-gray-800 hover:bg-gray-300'
+                    }`}
+                    onClick={() => setSelectedLetter(letter)}
+                  >
+                    {letter}
+                  </button>
+                ))}
+              </div>
             </div>
-            <hr className="border-gray-300 mb-4" />
-            <div className="flex flex-col space-y-3 text-gray-700 ml-2">
+            <div className="mb-3">
+              <h5 className="font-medium mb-2 text-gray-600">영문</h5>
+              <div className="flex flex-wrap gap-2">
+                {englishLetters.map((letter) => (
+                  <button
+                    key={letter}
+                    className={`w-10 h-10 flex items-center justify-center text-sm rounded-md transition-colors duration-300 ${
+                      selectedLetter === letter
+                        ? 'bg-blue-500 text-white border border-blue-700'
+                        : 'bg-gray-200 text-gray-800 hover:bg-gray-300'
+                    }`}
+                    onClick={() => setSelectedLetter(letter)}
+                  >
+                    {letter}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            {/* Rating Filter */}
+            <h4 className="font-semibold mb-2 text-gray-700">별점</h4>
+            <div className="relative">
               <button
-                className="text-left focus:text-blue-500 focus:font-bold"
-                onClick={() => handleSortChange('latest')}
+                className="w-full border p-2 px-4 rounded-lg text-m bg-white flex justify-between items-center"
+                onClick={() => setIsOpen(!isOpen)}
               >
-                최신순
+                {filter
+                  ? options.find((opt) => opt.value === filter)?.label
+                  : '별점 선택'}
+                <span className="ml-12">▼</span>
               </button>
-              <button className="text-left focus:text-blue-500 focus:font-bold">
-                평점순
-              </button>
-              <button className="text-left focus:text-blue-500 focus:font-bold">
-                리뷰순
-              </button>
-              <button
-                className="text-left focus:text-blue-500 focus:font-bold"
-                onClick={() => handleSortChange('pubDate')}
-              >
-                출간일순
-              </button>
+
+              {isOpen && (
+                <ul className="absolute w-full mt-1 border py-2 border-gray-300 rounded-lg bg-white shadow-md">
+                  {options.map((option) => (
+                    <li
+                      key={option.value}
+                      className="flex gap-1.5 items-center py-2 px-4 cursor-pointer hover:font-bold transition-all duration-300 text-[#FACC15]"
+                      onClick={() => {
+                        setFilter(option.value);
+                        setIsOpen(false);
+                      }}
+                    >
+                      {option.label}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+
+            {/* Date Filter */}
+            <h4 className="font-semibold mt-5 mb-2 text-gray-700">
+              출판일 기간
+            </h4>
+            <div className="flex flex-col gap-3">
+              <input
+                type="date"
+                className="border p-2 rounded-md w-full bg-white text-gray-800 focus:ring-2 focus:ring-blue-400"
+                value={startDate}
+                onChange={(e) => {
+                  setStartDate(e.target.value);
+                  setEndDate(''); // Reset the end date when start date changes
+                }}
+              />
+              <span className="text-center text-gray-600">~</span>
+              <input
+                type="date"
+                className="border p-2 rounded-md w-full bg-white text-gray-800 focus:ring-2 focus:ring-blue-400"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+                min={startDate}
+                disabled={!startDate}
+              />
             </div>
           </div>
 
-          {/* 책 목록 */}
+          {/* Books List */}
           <div className="w-6/7 overflow-y-auto max-h-[80vh] grid grid-cols-[repeat(3,minmax(0,0.5fr))] gap-12 ml-12 pr-11 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
             {sortedBooks.map((book) => (
               <div
-                key={book.isbn} // ISBN을 고유 키로 사용
+                key={book.isbn}
                 className="relative cursor-pointer book-card"
-                onClick={() => navigate(`/book/${book.isbn}`)} // 책 상세 페이지로 이동
+                onClick={() => navigate(`/book/${book.isbn}`)}
               >
-                <div
-                  className={`absolute top-0 right-2 cursor-pointer ${
-                    selectedBooks.has(book.isbn)
-                      ? 'text-green-500'
-                      : 'text-black/90'
-                  }`}
-                  onMouseDown={(e) => e.preventDefault()}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    toggleBookmark(book.isbn);
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-10 h-12"
-                    style={{
-                      filter:
-                        'drop-shadow(0.5px 2px 3px rgba(255, 255, 255, 0.5))',
-                    }}
-                  >
-                    <path d="M6 2a2 2 0 00-2 2v18l8-5 8 5V4a2 2 0 00-2-2H6z" />
-                  </svg>
-                </div>
                 <img
                   src={book.image}
                   alt={book.title}
