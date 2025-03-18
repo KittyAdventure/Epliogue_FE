77f0e1452d4739dddc266ca75de7faf0deb74cc1
cupnoodles37 <roopre48@daum.net>
2025-03-19 02:00:52 +0900

    :bug: fix: sockjs-client global 정의

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [tailwindcss()],
+  define: {
+    global: {},
+    'process.env': {},
+  },
 });
